8da8318815908801da396d2198652ddad935667d
jn-html <marmainjean@gmail.com>
2020-08-04 11:39:39 +0200

    First commit turn index.js into reusable components

---
+++ pages/about.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+// functional component - arrow function
+// const About = () => {
+//    const message = 'Hello world'
+//    return (
+//       <h1>hello from about - {message}</h1>
+//    )
+// }
+
+// const About = () => {
+//    const message = 'Hello world'
+//    return React.createElement('h1', null, 'I am genereting this with createElement')
+// }
+
+
+// unfunctionnal component - normal function
+// function About() {
+//    return (
+//       <h1>Hello from About Page</h1>
+//    )
+// }
+
+
+class About extends React.Component {
+   
+   render() {
+      return (
+         <h1>Hello i'm class a component</h1>
+      );
+   }
+}
+
+
+export default About;
+ 
