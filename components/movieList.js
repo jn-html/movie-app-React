04d06f672ea319de0a4ad1bfe217ba4a61ae0f06
jn-html <marmainjean@gmail.com>
2020-08-05 11:49:57 +0200

    Shorten Function

--- components/movieList.js
+++ components/movieList.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 class MovieList extends React.Component {
 
-	renderMovies(movies) {
-		
+	shorten = (text, maxLength) => {
+		if (text && text.length > maxLength) {
+			return text.substr(0, maxLength) + '...'
+		}
+		return text
+	}
 
+	renderMovies(movies) {
 		return movies.map(movie => 
 			(
 				<div className="col-lg-4 col-md-6 mb-4">
@@ -15,7 +20,7 @@ class MovieList extends React.Component {
 								<a href="#">{movie.name}</a>
 							</h4>
 							<h5>$24.99</h5>
-							<p className="card-text">{movie.description}</p>
+							<p className="card-text">{this.shorten(movie.description, 100)}</p>
 						</div>
 						<div className="card-footer">
 							<small className="text-muted">{movie.rating}</small>
