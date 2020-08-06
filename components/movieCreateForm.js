247f550a2bd56b647c52b7fc87325c50fc7bc679
jn-html <marmainjean@gmail.com>
2020-08-06 22:40:40 +0200

    Fix className in movieCreateForm.js

--- components/movieCreateForm.js
+++ components/movieCreateForm.js
@@ -4,34 +4,34 @@
 const MovieCreateForm = () => {
   return (
     <form>
-      <div class="form-group">
+      <div className="form-group">
         <label for="name">Name</label>
-        <input type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Lord of the Rings" />
+        <input type="text" className="form-control" id="name" aria-describedby="emailHelp" placeholder="Lord of the Rings" />
       </div>
-      <div class="form-group">
+      <div className="form-group">
         <label for="description">Description</label>
-        <input type="text" class="form-control" id="description" placeholder="Somewhere in Middle-earth..." />
+        <input type="text" className="form-control" id="description" placeholder="Somewhere in Middle-earth..." />
       </div>
-      <div class="form-group">
+      <div className="form-group">
         <label for="description">Rating</label>
-        <input type="number" max="5" min="0" class="form-control" id="rating" placeholder="3" />
-        <small id="emailHelp" class="form-text text-muted">Max: 5, Min: 0 </small>
+        <input type="number" max="5" min="0" className="form-control" id="rating" placeholder="3" />
+        <small id="emailHelp" className="form-text text-muted">Max: 5, Min: 0 </small>
       </div>
-      <div class="form-group">
+      <div className="form-group">
         <label for="image">Image</label>
-        <input type="text" class="form-control" id="image" placeholder="http://....." />
+        <input type="text" className="form-control" id="image" placeholder="http://....." />
       </div>
-      <div class="form-group">
+      <div className="form-group">
         <label for="cover">Cover</label>
-        <input type="text" class="form-control" id="cover" placeholder="http://......" />
+        <input type="text" className="form-control" id="cover" placeholder="http://......" />
       </div>
-      <div class="form-group">
+      <div className="form-group">
         <label for="longDesc">Long Description</label>
-        <textarea class="form-control" id="longDesc" rows="3"></textarea>
+        <textarea className="form-control" id="longDesc" rows="3"></textarea>
       </div>
-      <div class="form-group">
+      <div className="form-group">
         <label for="genre">Genre</label>
-        <select multiple class="form-control" id="genre">
+        <select multiple className="form-control" id="genre">
           <option>drama</option>
           <option>music</option>
           <option>adventure</option>
