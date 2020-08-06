3b675dd1452dcfd11ff44f58400ae389ffa3bcc3
jn-html <marmainjean@gmail.com>
2020-08-06 14:23:05 +0200

    Get Movie By Id

--- actions/index.js
+++ actions/index.js
@@ -36,3 +36,24 @@ export const getMovies = () => {
 		}, 50)
 	})
 }
+
+export const getMoviesById = (id) => {
+	return new Promise((resolve, reject) => {
+		const movieIndex = MOVIE_DATA.findIndex((movie) => {
+			return movie.id === id
+		})
+		const movie = MOVIE_DATA[movieIndex]
+
+		setTimeout(()=> resolve(movie) , 50)
+	})
+}
+
+// Another way to do the same as above
+// export const getMoviesById = (id) => {
+// 	return new Promise((resolve, reject) => {
+// 		const movieIndex = MOVIE_DATA.findIndex(m => m.id === id)
+// 		const movie = MOVIE_DATA[movieIndex]
+
+// 		setTimeout(()=> resolve(movie) , 50)
+// 	})
+// }
--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -1,16 +1,36 @@
 // Next offer bracket so id is dynamique
 import { useRouter } from 'next/router';
+import { getMoviesById } from '../../actions';
 
-const Movie = () => {
-   const router = useRouter()
+const Movie = (props) => {
+  const router = useRouter()
    // id here is named after the file's name
-   const { id } = router.query
+	const { id } = router.query
+	const { movie } = props
 
-   return (
+  return (
       <div className="container">
-         <h1>Movie with id: { id }</h1>
-      </div>
-   )
+        <div className="jumbotron">
+					<h1 className="display-4">{movie.name}</h1>
+					<p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+					<hr className="my-4"/>
+					<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+					<p className="lead">
+    			<a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  				</p>
+      	</div>
+				<p>
+					Some description about the movie
+				</p>
+			</div>
+  )
+}
+
+// Call getMovieById ("2")
+Movie.getInitialProps = async () => {
+	const movie = await getMoviesById("2")
+	
+	return { movie }
 }
 
 export default Movie;
