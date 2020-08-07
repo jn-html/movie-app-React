3a64ffc892bb4e503ddfd51a8a974bce24a63c80
jn-html <marmainjean@gmail.com>
2020-08-07 13:01:27 +0200

    Base server implementation

---
+++ server/index.js
@@ -0,0 +1,25 @@
+
+
+const next = require('next')
+const express = require('express');
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+
+  const server = express();
+  // We are handling all requests comming to our server
+  server.get('*', (req, res) => {
+    // next.js is handling requests and providing pages we are navigating to
+    return handle(req, res)
+  })
+
+  const PORT = process.env.PORT || 3000;
+
+  server.use(handle).listen(PORT, (err) => {
+    if (err) throw err
+    console.log('> Ready on port ' + PORT)
+  })
+})
